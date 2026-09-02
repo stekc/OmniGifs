6cbdafd01418e26a5b1ff7d72a2d89837ea826c9
iCrazeiOS <39101269+iCrazeiOS@users.noreply.github.com>
2026-09-02 03:31:03 +0100

    Allow macOS shortcuts such as cmd+c to work within the search box

--- Sources/OmniGifs/UI/CommandSearchField.swift
+++ Sources/OmniGifs/UI/CommandSearchField.swift
@@ -5,11 +5,20 @@ final class CommandSearchField: NSSearchField {
         let modifiers = event.modifierFlags.intersection([
             .command, .option, .control, .shift,
         ])
-        guard modifiers == .command,
-            event.charactersIgnoringModifiers?.lowercased() == "a"
-        else {
+        guard modifiers == .command || modifiers == [.command, .shift] else {
             return super.performKeyEquivalent(with: event)
         }
+
+        let action: Selector?
+        switch event.charactersIgnoringModifiers?.lowercased() {
+        case "x" where modifiers == .command: action = #selector(NSText.cut(_:))
+        case "c" where modifiers == .command: action = #selector(NSText.copy(_:))
+        case "v" where modifiers == .command: action = #selector(NSText.paste(_:))
+        case "a" where modifiers == .command: action = #selector(NSText.selectAll(_:))
+        case "z": action = Selector(modifiers.contains(.shift) ? "redo:" : "undo:")
+        default: action = nil
+        }
+        guard let action else { return super.performKeyEquivalent(with: event) }
 
         if currentEditor() == nil {
             window?.makeFirstResponder(self)
@@ -17,7 +26,6 @@ final class CommandSearchField: NSSearchField {
         guard let editor = currentEditor() else {
             return super.performKeyEquivalent(with: event)
         }
-        editor.selectAll(nil)
-        return true
+        return NSApp.sendAction(action, to: editor, from: self)
     }
 }
